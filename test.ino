86950db283a1b028143d0587c2b48bad07da5d29
Godnaria <godnaria5182@gmail.com>
2020-12-04 09:30:12 +0800

    hi

--- test.ino
+++ test.ino
@@ -1,16 +1,13 @@
+int t;
+
 void setup()
 {
-    pinMode(13, OUTPUT);
+    pinMode(A0, INPUT);
+    Serial.begin(9600);
+    Serial.println("hello");
 }
 
 void loop()
 {
-    digitalWrite(13, HIGH);
-    delay(250);
-    digitalWrite(13, LOW);
-    delay(250);
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+    
 }
