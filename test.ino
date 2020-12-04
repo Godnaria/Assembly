bc7702e2a9450179c2b0874b8f6819c015dad9bd
Godnaria <godnaria5182@gmail.com>
2020-12-04 10:28:11 +0800

    HCSR04

--- test.ino
+++ test.ino
@@ -1,13 +1,17 @@
-int t;
+#include <HCSR04.h>
 
-void setup()
-{
-    pinMode(A0, INPUT);
-    Serial.begin(9600);
-    Serial.println("hello");
+// Initialize sensor that uses digital pins 13 and 12.
+int triggerPin = 8;
+int echoPin = 10;
+UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
+
+void setup () {
+    Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
 }
 
-void loop()
-{
-    
+void loop () {
+    // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
+    double distance = distanceSensor.measureDistanceCm();
+    Serial.println(distance);
+    delay(500);
 }
