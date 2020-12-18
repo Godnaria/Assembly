39180cfa4f1512c2153b62dbc5cb09ea01147c13
daven3302 <daven3302@gmail.com>
2020-12-18 10:49:13 +0800

    define pin , and define motor function

--- motor.h
+++ motor.h
@@ -1,1 +1,21 @@
 //this file contain the pin of motors' definition
+#define MIDDLE_ECHO 2
+#define MIDDLE_TRIG 4
+#define LEFT_ECHO 7
+#define LEFT_TRIG 8
+#define RIGHT_ECHO 10
+#define RIGHT_TRIG 11
+#define MOTOR_A1 3      //HIGH:forward LOW:backward
+#define MOTOR_B1 5      //speed 0~255
+#define MOTOR_A2 6
+#define MOTOR_B2 9
+typedef int Motor;
+void turn_left(Motor ax = MOTOR_A1,Motor bx = MOTOR_B1 , int speed)
+{
+    digitalWrite(MOTOR_B1,speed);
+}
+
+void turn_right(Motor ax = MOTOR_A2,Motor bx = MOTOR_B2 , int speed)
+{
+    digitalWrite(MOTOR_B2,speed);
+}
--- test.ino
+++ test.ino
@@ -1,17 +1,38 @@
 #include <HCSR04.h>
+#include "motor.h"
 
-// Initialize sensor that uses digital pins 13 and 12.
-int triggerPin = 8;
-int echoPin = 10;
-UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
+UltraSonicDistanceSensor distanceSensor_middle(MIDDLE_TRIG, MIDDLE_ECHO);
+UltraSonicDistanceSensor distanceSensor_left(LEFT_TRIG, LEFT_ECHO);
+UltraSonicDistanceSensor distanceSensor_right(RIGHT_TRIG, RIGHT_ECHO);
 
-void setup () {
+void setup () 
+{
     Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
+    pinMode(MIDDLE_TRIG,INPUT);
+    pinMode(MIDDLE_ECHO,INPUT);
+    pinMode(LEFT_TRIG,INPUT);
+    pinMode(LEFT_ECHO,INPUT);
+    pinMode(RIGHT_TRIG,INPUT);
+    pinMode(RIGHT_ECHO,INPUT);
+    pinMode(MOTOR_A1,OUTPUT);
+    pinMode(MOTOR_A2,OUTPUT);
+    pinMode(MOTOR_B1,OUTPUT);
+    pinMode(MOTOR_B2,OUTPUT);
 }
 
-void loop () {
+void loop () 
+{
+    digitalWrite(MOTOR_B1,255);
+    digitalWrite(MOTOR_B2,255);
     // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
-    double distance = distanceSensor.measureDistanceCm();
+    double distance = distanceSensor_middle.measureDistanceCm();
+    if(distance <= 20)
+    {
+        if(distanceSensor_left.measureDistanceCm() <= 20)
+            turn_right(200);
+        else 
+            turn_left(200);
+    }
     Serial.println(distance);
     delay(500);
 }
